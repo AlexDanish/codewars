9f4791044288b796c68ebce3b90f0902fd92b7f1
Алексей Еременко <138244975+AlexDanish@users.noreply.github.com>
2023-11-29 22:51:46 +0600

    Add files via upload

---
+++ Find the smallest integer in the array.py
@@ -0,0 +1,8 @@
+print('Find the smallest integer in the array')
+
+#Given an array of integers your solution should 
+# find the smallest integer.
+
+def find_smallest_int(arr):
+    result = min (arr)
+    return result
